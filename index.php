d9aecf6a2b7c08fc9f544fedcfbef473dab2f14f
Joan Osorio <joan_osorio28@outlook.com>
2024-07-08 15:32:53 -0500

    Estructura Principal y Conexiones a BD

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="estilos/styles.css">
+    </head>
+    <body>
+        <div class="main-container">
+            
+            <div class="container">
+                <button type="button" class="btn btn-default btn-back">← Atrás</button>
+                <h3 >Por favor, ingrese su número de cédula y tipo de documento</h3>
+                <div class="container-inputs">  
+                    <h5>Tipo de documento</h5>
+                    <select class="form-control">
+                        <option>Tipo de documento</option>
+                        <option>Cédula de Ciudadanía</option>
+                        <option>Tarjeta de Identidad</option>
+                        <option>Cédula de Extranjería</option>
+                        <option>Pasaporte</option>
+                    </select><br>
+                    
+                    <h5>Número de documento</h5>
+                    <input type="text" placeholder="Número de documento" class="form-control" ><br>
+                    <button type="button" class="btn btn-primary" style="width: 100%;">Buscar cita</button>
+                    <div class="captcha-btn">
+                        <h6>Aquí va el supuesto captcha</h6>
+                    </div>
+                </div>
+                
+            </div>
+           
+        </div>
+        <div class="bear-image"></div>
+        <div class="logos-image"></div>
+        <div class="logo-hdtuu-image"></div>
+    </body>
+</html>
