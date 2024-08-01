24f39947a131d87a6d5745c1e245a18806e4b755
Joan Osorio <joan_osorio28@outlook.com>
2024-08-01 17:05:38 -0500

    Consumo de API

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const sqlConfig = {
         encrypt: false
     }
 };
+
 const mysqlConfig = {
     host: process.env.MYSQL_HOST,
     port: parseInt(process.env.MYSQL_PORT),
@@ -28,12 +29,39 @@ async function connectSqlServer() {
     try {
         await pool.connect();
         console.log('Conexión establecida con SQLServer');
+
+        const today = new Date().toISOString().split('T')[0];
+
+        const consulta = 
+            `SELECT c.citnum NumCitPac, c.cittipdoc TipDocPac, c.citced NumDocPac, cb.MPNOMC NombPac, 
+                CASE 
+                    WHEN c.citesta = 'R' THEN 'RESERVADA' 
+                    WHEN c.citesta = 'C' THEN 'CONFIRMADA' 
+                    WHEN c.citesta = 'I' THEN 'INCUMPLIDA' 
+                    WHEN c.citesta = 'N' THEN 'CANCELADA' END EstCitPac,
+                m.menome NomEspCit, cast(c.CitFecPa as date) FchCitPac, CitHorIPa HorCitPac, md.mmnomm NomEspcCit, 
+                CitProCod CodProCit, mp.prnomb NomProCit
+                FROM citmed1 c
+                INNER JOIN citmed2 c2 ON (c.citnum = c2.citnum AND c.citemp = c2.citemp AND c.citsed = c2.citsed)
+                INNER JOIN citmed3 c3 ON (c.citnum = c3.citnum AND c.citemp = c3.citemp AND c.citsed = c3.citsed)
+                INNER JOIN maeesp m ON (c2.mecode = m.mecode)
+                INNER JOIN maemed1 md ON (c2.MMCODM = md.mmcodm)
+                INNER JOIN CAPBAS cb ON (c.citced = cb.MPCedu AND c.cittipdoc = cb.MPTDoc) 
+                INNER JOIN maepro mp ON (c3.citprocod = mp.prcodi)
+                WHERE c.citfecpa >= '${today}' AND c.citesta IN ('R','C','I','N')
+                ORDER BY 1`;
+
+        const result = await pool.request().query(consulta);
+        const citas = result.recordset;
+
+        await insertIntoMysql(citas);
+
     } catch (error) {
         console.error('Error al conectar con SQLServer', error);
     }
 }
 
-async function connectToMysql() {
+async function insertIntoMysql(citas) {
     try {
         const connection = await new Promise((resolve, reject) => {
             poolMysql.getConnection((error, connection) => {
@@ -46,8 +74,21 @@ async function connectToMysql() {
         });
 
         console.log('Conexión establecida con MySQL');
-        
-        // 
+
+        const insertQuery = `INSERT INTO Citas (NumCitPac, TipDocPac, NumDocPac, NombPac, EstCitPac, NomEspCit, FchCitPac, HorCitPac, NomEspcCit, CodProCit, NomProCit) VALUES ?`;
+
+        const values = citas.map(cita => [
+            cita.NumCitPac, cita.TipDocPac, cita.NumDocPac, cita.NombPac, cita.EstCitPac, cita.NomEspCit, 
+            cita.FchCitPac, cita.HorCitPac, cita.NomEspcCit, cita.CodProCit, cita.NomProCit
+        ]);
+
+        connection.query(insertQuery, [values], (error, results) => {
+            if (error) {
+                console.error('Error al insertar en MySQL', error);
+            } else {
+                console.log('Datos insertados en MySQL', results);
+            }
+        });
 
         connection.release(); // Liberar la conexión después de usarla
     } catch (error) {
@@ -56,4 +97,3 @@ async function connectToMysql() {
 }
 
 connectSqlServer();
-connectToMysql();
--- index.php
+++ index.php
@@ -1,40 +1,81 @@
-<html>
-    <head>
-        <title>Welcome to LAMP Infrastructure</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="estilos/styles.css">
-    </head>
-    <body>
-        <div class="main-container">
-            
-            <div class="container">
-                <button type="button" class="btn btn-default btn-back">← Atrás</button>
-                <h3 >Por favor, ingrese su número de cédula y tipo de documento</h3>
-                <div class="container-inputs">  
-                    <h5>Tipo de documento</h5>
-                    <select class="form-control">
-                        <option>Tipo de documento</option>
-                        <option>Cédula de Ciudadanía</option>
-                        <option>Tarjeta de Identidad</option>
-                        <option>Cédula de Extranjería</option>
-                        <option>Pasaporte</option>
-                    </select><br>
-                    
-                    <h5>Número de documento</h5>
-                    <input type="text" placeholder="Número de documento" class="form-control" ><br>
-                    <button type="button" class="btn btn-primary" style="width: 100%;">Buscar cita</button>
-                    <div class="captcha-btn">
-                        <h6>Aquí va el supuesto captcha</h6>
-                    </div>
-                </div>
-                
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Citas Medicas Tomas</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos/styles.css">
+    <script src="https://www.google.com/recaptcha/enterprise.js?render=6LeV-QoqAAAAABw3V7KI_B40KowW_pRQtK6lHse3"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div id="modal" class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-content" id="modalContent">
+            <span class="close-btn" id="closeModalBtn">&times;</span>
+            <div class="icon">
+                <img src="https://example.com/warning-icon.png" alt="Warning Icon">
             </div>
-           
+            <h2>USUARIO NO ENCONTRADO</h2>
+            <p>El usuario ingresado no se encuentra en la base de datos.<br>Por favor, verifique los datos e inténtelo de nuevo.</p>
+            <button id="acceptBtn">Aceptar</button>
         </div>
-        <div class="bear-image"></div>
-        <div class="logos-image"></div>
-        <div class="logo-hdtuu-image"></div>
-    </body>
+    </div>
+    <div class="main-container">
+        <div class="container">
+            <button type="button" class="btn btn-default btn-back">← Atrás</button>
+            <h3>Por favor, ingrese su número de cédula y tipo de documento</h3>
+            <div class="container-inputs">  
+                <h5>Tipo de documento</h5>
+                <select class="form-control">
+                    <option>Tipo de documento</option>
+                    <option>Cédula de Ciudadanía</option>
+                    <option>Tarjeta de Identidad</option>
+                    <option>Cédula de Extranjería</option>
+                    <option>Pasaporte</option>
+                </select><br>
+                
+                <h5>Número de documento</h5>
+                <input type="text" placeholder="Número de documento" class="form-control" ><br>
+                <button type="button" class="btn btn-primary" id="showModalBtn" style="width: 100%;">Buscar cita</button>
+                <div class="captcha-btn">
+                    <!--div class="g-recaptcha" data-sitekey="6LeV-QoqAAAAABw3V7KI_B40KowW_pRQtK6lHse3"></!--div-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bear-image"></div>
+    <div class="logos-image"></div>
+    <div class="logo-hdtuu-image"></div>
+
+    <script>
+        function onClick(e) {
+            e.preventDefault();
+            grecaptcha.enterprise.ready(async () => {
+                const token = await grecaptcha.enterprise.execute('6LeV-QoqAAAAABw3V7KI_B40KowW_pRQtK6lHse3', {action: 'LOGIN'});
+            });
+        }
+
+        document.getElementById('showModalBtn').addEventListener('click', function() {
+            document.getElementById('modal').style.display = 'flex';
+            document.getElementById('modalContent').classList.add('slide-in');
+        });
+
+        document.getElementById('closeModalBtn').addEventListener('click', function() {
+            document.getElementById('modal').style.display = 'none';
+        });
+
+        document.getElementById('acceptBtn').addEventListener('click', function() {
+            document.getElementById('modal').style.display = 'none';
+        });
+
+        // Close the modal if the user clicks anywhere outside of the modal
+        window.onclick = function(event) {
+            if (event.target == document.getElementById('modal')) {
+                document.getElementById('modal').style.display = 'none';
+            }
+        }
+    </script>
+</body>
 </html>
